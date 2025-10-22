c8a3c708e295fb2c12692f113cddaffa8534f4f7
Jeremy Fowler <jembyfowler@gmail.com>
2025-10-22 09:46:22 -0600

    restaurant model

---
+++ server/Models/Restaurant.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace help_reports.Models;
+
+public class Restaurant : RepoItem<int>
+{
+  public string Name { get; set; }
+  [Url] public string ImgUrl { get; set; }
+  public string Description { get; set; }
+  public int Visits { get; set; }
+  public bool IsShutdown { get; set; }
+  public string CreatorId { get; set; }
+  public Profile Owner { get; set; }
+  // TODO make sure this actually implemented in the repository
+  public int ReportCount { get; set; }
+}
