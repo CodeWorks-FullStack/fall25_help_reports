592b3cbc967fe6210564c8d6d35efd6ca5bb2cd2
Jeremy Fowler <jembyfowler@gmail.com>
2025-10-22 11:24:59 -0600

    getting restaurantz

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,13 +1,30 @@
-<script setup >
-import Example from '@/components/Example.vue';
+<script setup>
+import { restaurantsService } from '@/services/RestaurantsService.js';
+import { logger } from '@/utils/Logger.js';
+import { Pop } from '@/utils/Pop.js';
+import { onMounted } from 'vue';
 
+onMounted(getRestaurants)
+
+async function getRestaurants() {
+  try {
+    await restaurantsService.getRestaurants()
+  } catch (error) {
+    Pop.error(error)
+    logger.error('COULD NOT GET RESTAURANTS', error)
+  }
+}
 
 </script>
 
 <template>
-  <Example />
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-4">
+
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped lang="scss">
-
-</style>
+<style scoped lang="scss"></style>
---
+++ client/src/services/RestaurantsService.js
@@ -0,0 +1,10 @@
+import { logger } from "@/utils/Logger.js"
+import { api } from "./AxiosService.js"
+
+class RestaurantsService {
+  async getRestaurants() {
+    const response = await api.get('api/restaurants')
+    logger.log('GOT RESTAURANTS 🏪🏪🏪🏪', response.data)
+  }
+}
+export const restaurantsService = new RestaurantsService()
