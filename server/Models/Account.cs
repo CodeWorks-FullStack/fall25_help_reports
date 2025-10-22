d9158af55904019f162a3b1b0ae524d5bdde0794
Jeremy Fowler <jembyfowler@gmail.com>
2025-10-22 09:30:02 -0600

    init

---
+++ server/Models/Account.cs
@@ -0,0 +1,9 @@
+namespace help_reports.Models;
+
+public class Account
+{
+  public string Id { get; set; }
+  public string Name { get; set; }
+  public string Email { get; set; }
+  public string Picture { get; set; }
+}
